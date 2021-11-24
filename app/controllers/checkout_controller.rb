ee6e906f3e12e403139ca3ca8d412fe4cf7a0de4
Lawrence Valerio <valerio.lawrence@gmail.com>
2021-11-24 13:27:56 -0600

    Styled login and register page.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,2 +1,48 @@
 class CheckoutController < ApplicationController
+  def create
+    product = Product.find(params[:card_id])
+    if product.nil?
+      redirect_to cards
+      return
+    end
+
+    @session = Stripe::Checkout::Session.create(
+      # went to stripe API, looked up sessions, figured it all out..
+      payment_method_types: ['card'],
+      success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: checkout_cancel_url,
+      line_items: [
+        {
+          name: product.name,
+          description: product.description,
+          amount: product.price_cents,
+          currency: 'cad',
+          quantity: 1 # We will hardcode one for this demo.
+        },
+        {
+          name: 'GST',
+          description: 'Goods and Services Tax',
+          amount: (product.price_cents * 0.05).to_i,
+          currency: 'cad',
+          quantity: 1
+        }
+      ]
+    )
+
+    respond_to do |format|
+      format.js # render app/views/checkout/create.js.erb
+    end
+  end
+
+  def success
+    # stripe success_url +"?session_id={CHECKOUT_SESSION_ID}"
+    # when stripe redirects back to server... it will append this session_id  through GET params!
+    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+    # found in the docs!
+  end
+
+  def cancel
+    # something went wrong :(
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   get 'search', to: 'cards#search'
   resources :cards, only: %i[index show]
   resources :types, only: %i[index show]
+  resources :login
+  resources :register
   resources :rarities, only: %i[index show]
+
+  scope '/checkout' do
+    post 'create',  to: 'checkout#create',  as: 'checkout_create'
+    get  'success', to: 'checkout#success', as: 'checkout_success'
+    get  'cancel',  to: 'checkout#cancel',  as: 'checkout_cancel'
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
